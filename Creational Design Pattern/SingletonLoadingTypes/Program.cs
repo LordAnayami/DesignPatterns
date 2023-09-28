84623dca5ca18d41dce2711f9add3cbcb5be2c08
lordanayami <matrixwenta92@interia.pl>
2023-09-28 18:25:29 +0200

    SingletonPart1

---
+++ Creational Design Pattern/SingletonLoadingTypes/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+namespace SingletonLoadingTypes
+{
+    class Program
+    {
+        //Example to Understand Thraed-Safe Singleton Design Pattern using Eager Loading
+        //When use in a Multithreaded Environment
+        static void Main(string[] args)
+        {
+            //The following Code will Invoke both methods Parallely using two different Threads
+            Parallel.Invoke(
+                //Let us Assume PrintTeacherDetails method is Invoked by Thread-1
+                () => PrintTeacherDetails(),
+                //Let us Assume PrintStudentdetails method is Invoked by Thread-2
+                () => PrintStudentdetails()
+                );
+            Console.ReadLine();
+        }
+
+        private static void PrintTeacherDetails()
+        {
+            //Thread-1 Calling the GetInstance() Method of the Singleton class
+            Singleton fromTeacher = Singleton.GetInstance();
+            fromTeacher.PrintDetails("From Teacher");
+        }
+
+        private static void PrintStudentdetails()
+        {
+            //At the same time, Thread-2 also Calling the GetInstance() Method of the Singleton Class
+            Singleton fromStudent = Singleton.GetInstance();
+            fromStudent.PrintDetails("From Student");
+        }
+    }
+}
---
+++ Creational Design Pattern/SingletonTreadSafe/SingletonNoProtected.cs
@@ -0,0 +1,44 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonThreadSafe
+{
+    public sealed class Singleton
+    {
+        //This variable value will be increment by 1 each time the object of the class is created
+        private static int Counter = 0;
+
+        //This variable is going to store the Singleton Instance
+        private static Singleton Instance = null;
+
+        //The following Static Method is going to return the Singleton Instance
+        public static Singleton GetInstance()
+        {
+            //This is not thread-safe
+            if (Instance == null)
+            {
+                Instance = new Singleton();
+            }
+
+            //Return the Singleton Instance
+            return Instance;
+        }
+
+        private Singleton()
+        {
+            //Each Time the Constructor is called, increment the Counter value by 1
+            Counter++;
+            Console.WriteLine("Counter Value " + Counter.ToString());
+        }
+
+        //The following method can be accessed from outside of the class by using the Singleton Instance
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
+*/

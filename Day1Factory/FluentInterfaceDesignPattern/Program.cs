4ff3694d4eee6d624aae1c38be8be6f8aaec7982
lordanayami <matrixwenta92@interia.pl>
2023-09-28 13:31:26 +0200

    day2end

---
+++ Day1Factory/FluentInterfaceDesignPattern/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace FluentInterfaceDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create an Instance of Wrapper class i.e. FluentEmployee
+            FluentEmployee obj = new FluentEmployee();
+
+            //Call Methods one by one using dot Operator whose Return Type is FluentEmployee
+            obj.NameOfTheEmployee("Anurag Mohanty")
+                    .Born("10/10/1992")
+                    .WorkingOn("IT")
+                    .StaysAt("Mumbai-India");
+
+            //To See the Details call the ShowDetails Method
+            obj.ShowDetails();
+            Console.Read();
+        }
+    }
+}

9d2b9b9c2bb602e56a3e21422be1cdf91c80d8d3
lordanayami <matrixwenta92@interia.pl>
2023-10-02 19:15:43 +0200

    structural end

---
+++ Structural Design Patterns/CompositeDesignPattern/Leaf.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompositeDesignPattern
+{
+    // The Leaf class represents the end objects. 
+    // A leaf can't have any children.
+    // The Leaf object is the Object which does the actual work
+    public class Leaf : IComponent
+    {
+        public int Price { get; set; }
+        public string Name { get; set; }
+
+        public Leaf(string name, int price)
+        {
+            this.Price = price;
+            this.Name = name;
+        }
+
+        public void DisplayPrice()
+        {
+            Console.WriteLine($"\tComponent Name: {Name} and Price: {Price}");
+        }
+    }
+}
---
+++ Structural Design Patterns/DecoratorDesignPatternRealTimeExample/PlainPizza.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecoratorDesignPatternRealTimeExample
+{
+    // Concrete Components provide default implementations of the operations.
+    public class PlainPizza : Pizza
+    {
+        //The following MakePizza method returns the default Pizza
+        public string MakePizza()
+        {
+            return "Plain Pizza";
+        }
+    }
+}
